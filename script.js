05336cbddd783458b7c8f162818cdeea3e38ab31
Vladislav Vlasenko <you@example.comvlad-vlasenko@mail.ru>
2022-06-28 18:55:53 +0300

    done second task

--- script.js
+++ script.js
@@ -27,3 +27,12 @@
 
 const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
+//2
+
+const personalMovieDB = {
+    count: +numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
